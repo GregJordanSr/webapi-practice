62bab29a7b4d31073185bb3da127ed36f9d1250d
Greg Jordan <gregljordan@gmail.com>
2019-08-08 13:13:42 -0400

    added process.env

--- index.js
+++ index.js
@@ -5,4 +5,4 @@
 const server =  require('./api/server.js');
 
 const port = process.env.PORT || 8000;
-server.listen(port, () => console.log('\n***API on ${port}\n'));
+server.listen(port, () => console.log(`\n***API on ${port}***\n`));
