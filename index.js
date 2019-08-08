94471b8ac00acb90a47db03f3b26468b8354b3b8
Greg Jordan <gregljordan@gmail.com>
2019-08-08 13:11:42 -0400

    added process.env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //separation of concerns (Soc)
 // single responsibility principle (SRP)
 //  - each module or file has a single responsibility
-const server =  require('./api/server');
+const server =  require('./api/server.js');
 
-server.listen(port, () => console.log('\n***Server is running on http://localhost:8000\n'));
+const port = process.env.PORT || 8000;
+server.listen(port, () => console.log('\n***API on ${port}\n'));
